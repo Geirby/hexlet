7ee57177545bb4ce8e3355e69e376c95bdb12831
Evgeny I <xzenya@gmail.com>
2017-07-27 15:34:22 +0300

    binary sum

--- practice/php_basics_binary_sum.php
+++ practice/php_basics_binary_sum.php
@@ -1,9 +1,9 @@
 <?php
 /**
-Реализуйте функцию binarySum, которая складывает переданные бинарные числа (как строки):
-
-'11' == binarySum('10', '01');
-'10010' == binarySum('1101', '101');
+ * Реализуйте функцию binarySum, которая складывает переданные бинарные числа (как строки):
+ *
+ * '11' == binarySum('10', '01');
+ * '10010' == binarySum('1101', '101');
  */
 
 namespace App\Solution;
@@ -11,20 +11,59 @@ namespace App\Solution;
 // BEGIN (write your solution here)
 function binarySum($a, $b)
 {
-    $countA = strlen($a);
-    for ($i = 1; $i <= $countA; $i++) {
-       echo substr($a, $countA - $i, 1) . "\n";
+    //first step: what length of string is longer? Equalise it.
+    if (strlen($a) > strlen($b)) {
+        $addZeroCount = strlen($a) - strlen($b);
+        $addZero = '';
+        for ($j = 0; $j < $addZeroCount; $j++) {
+            $addZero .= '0';
+        }
+        $b = $addZero . $b;
+    } else {
+        $addZeroCount = strlen($b) - strlen($a);
+        $addZero = '';
+        for ($j = 0; $j < $addZeroCount; $j++) {
+            $addZero .= '0';
+        }
+        $a = $addZero . $a;
     }
 
-    return ;
+    $longerCount = (strlen($a) >= strlen($b)) ? strlen($a) - 1 : strlen($b) - 1;
+    //second step: get both string from last symbols
+    $result = '';
+    $tmpVar = 0;
+    for ($i = 0; $i <= $longerCount; $i++) {
+
+        $firstNum = substr($a, $longerCount - $i, 1);
+        $secondNum = substr($b, $longerCount - $i, 1);
+        $sum = $firstNum + $secondNum + $tmpVar;
+        echo $sum . PHP_EOL;
+        switch ($sum) {
+            case 0 :
+                $result .= '0';
+                $tmpVar = 0;
+                break;
+            case 1 :
+                $result .= '1';
+                $tmpVar = 0;
+                break;
+            case 2:
+                $result .= '0';
+                $tmpVar = 1;
+                break;
+            case 3:
+                $result .= '1';
+                $tmpVar = 1;
+                break;
+        }
+    }
+    $addResult = ($tmpVar) ? $tmpVar : '';
+    $result = strrev($result . $addResult);
+//    echo 'result = ' . $result . PHP_EOL;
+    return $result;
 }
 // END
 
-print_r(binarySum('110100', '1101'));
-//10 = 2
-//11 = 3
-//100 = 4
-//101 = 5
-//    110 = 6
-//    111 = 7
-//    1000 = 8
+//example
+//print_r(binarySum('10', '1'));
+
